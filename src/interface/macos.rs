85bb3131873f223f8dea3bdf0e3f5cd4ab5ee189
shellrow <81893184+shellrow@users.noreply.github.com>
2022-03-28 21:40:38 +0900

    Update macos.rs

--- src/interface/macos.rs
+++ src/interface/macos.rs
@@ -15,8 +15,16 @@ pub fn get_if_type_map() ->  HashMap<String, InterfaceType> {
     let mut map: HashMap<String, InterfaceType> = HashMap::new();
     let interfaces = network_configuration::get_interfaces();
     for interface in &interfaces {
-        let if_name: String = interface.bsd_name().unwrap().to_string();
-        let type_id: String = interface.interface_type_string().unwrap().to_string();
+        let if_name: String = if let Some(bsd_name) = interface.bsd_name() {
+            bsd_name.to_string()
+        }else{
+            continue;
+        };
+        let type_id: String = if let Some(type_string) = interface.interface_type_string() {
+            type_string.to_string()
+        }else{
+            continue;
+        };
         map.insert(if_name, get_if_type_from_id(type_id));
     }
     return map;
