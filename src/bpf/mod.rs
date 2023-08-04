694d79a85a43fe6c69072b22aaf8a9c02e9557a1
shellrow <81893184+shellrow@users.noreply.github.com>
2023-08-04 22:05:11 +0900

    Refactoring bpf module

--- src/bpf/mod.rs
+++ src/bpf/mod.rs
@@ -7,20 +7,3 @@ pub use self::binding::*;
 mod unix;
 #[cfg(any(target_os = "openbsd", target_os = "freebsd", target_os = "netbsd"))]
 pub use self::unix::*;
-
-#[cfg(any(
-    target_os = "macos",
-    target_os = "openbsd",
-    target_os = "freebsd",
-    target_os = "netbsd",
-    target_os = "ios"
-))]
-mod shared;
-#[cfg(any(
-    target_os = "macos",
-    target_os = "openbsd",
-    target_os = "freebsd",
-    target_os = "netbsd",
-    target_os = "ios"
-))]
-pub use self::shared::*;
