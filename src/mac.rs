f870da9dc4d38d1bbb1b8a2bfdabfb938f23a031
shellrow <81893184+shellrow@users.noreply.github.com>
2023-11-04 17:59:25 +0900

    Change MacAddr serde impl
     for human-readable hex format support

--- src/mac.rs
+++ src/mac.rs
@@ -3,11 +3,10 @@ use std::fmt;
 use core::str::FromStr;
 
 #[cfg(feature = "serde")]
-use serde::{Deserialize, Serialize};
+use serde::{Serialize, Serializer, Deserialize, Deserializer, de};
 
 /// Structure of MAC address
 #[derive(Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash, Default, Debug)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct MacAddr(pub u8, pub u8, pub u8, pub u8, pub u8, pub u8);
 
 impl MacAddr {
@@ -64,6 +63,55 @@ impl std::fmt::Display for MacAddr {
             self.0, self.1, self.2, self.3, self.4, self.5
         );
         Ok(())
+    }
+}
+
+#[cfg(feature = "serde")]
+impl Serialize for MacAddr {
+    /// Serializes the MAC address.
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        if serializer.is_human_readable() {
+            serializer.collect_str(self)
+        } else {
+            serializer.serialize_bytes(&[self.0, self.1, self.2, self.3, self.4, self.5])
+        }
+    }
+}
+
+#[cfg(feature = "serde")]
+impl<'de> Deserialize<'de> for MacAddr {
+    /// Deserializes the MAC address.
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        struct MacAddrVisitor;
+        impl<'de> de::Visitor<'de> for MacAddrVisitor {
+            type Value = MacAddr;
+
+            fn visit_str<E: de::Error>(self, value: &str) -> Result<MacAddr, E> {
+                value.parse().map_err(|err| E::custom(err))
+            }
+
+            fn visit_bytes<E: de::Error>(self, v: &[u8]) -> Result<MacAddr, E> {
+                if v.len() == 6 {
+                    Ok(MacAddr::new(v[0], v[1], v[2], v[3], v[4], v[5]))
+                } else {
+                    Err(E::invalid_length(v.len(), &self))
+                }
+            }
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                write!(
+                    formatter,
+                    "either a string representation of a MAC address or 6-element byte array"
+                )
+            }
+        }
+
+        // Decide what hint to provide to the deserializer based on if it is human readable or not
+        if deserializer.is_human_readable() {
+            deserializer.deserialize_str(MacAddrVisitor)
+        } else {
+            deserializer.deserialize_bytes(MacAddrVisitor)
+        }
     }
 }
 
